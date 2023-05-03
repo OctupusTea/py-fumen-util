4e13b48d0ee5f6198f7b1aedec2a209742ef5e84
OctupusTea <eland.tea@gmail.com>
2023-05-03 17:17:44 +0800

    Added gray and grayall to __init__.py

--- src/py_fumen_util/__init__.py
+++ src/py_fumen_util/__init__.py
@@ -4,6 +4,8 @@ from .fumen_combiner import fumen_combiner
 from .fumen_locker import fumen_locker
 from .fumen_splitter import fumen_splitter
 from .glue_fumen import glue_fumen
+from .grayout_all import grayout_all_fumen
+from .grayout_last import grayout_fumen
 from .mirror_fumen import mirror_fumen
 from .remove_comment import remove_comment
 from .unglue_fumen import unglue_fumen
